c072983d1caf149df34beb032c441008c76c8c9c
LucasRafaelBalduino <42643459+LucasRafaelBalduino@users.noreply.github.com>
2019-09-27 11:28:26 -0300

    Estrutura Frontend

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './global.css'; // importanto css
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
